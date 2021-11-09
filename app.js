6825e4e18c43bbeb1a14b89b70547032a3c8f8dd
jifwastaken <72139743+jifwastaken@users.noreply.github.com>
2021-11-09 11:39:21 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result");
+const rock_div = document.getElementById("r")
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices [randomNumber];
+}
+
+function win() {
+    console.log("WIN");
+}
+
+function lose() {
+    console.log("LOST");
+}
+
+function draw() {
+    console.log("DRAWWW");
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win();
+            break;
+        case "r":
+        case "ps":
+        case "sr":
+            lose();
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw();
+            break;
+    }
+}
+
+game("c");
+
+function main() {
+    
+}
